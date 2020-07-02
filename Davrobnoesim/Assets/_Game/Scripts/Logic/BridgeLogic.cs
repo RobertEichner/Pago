a577e8788294553ce59d1e239e7d95a1c29bdd27
Simon Metzger <metzgers@hochschule-trier.de>
2020-07-02 15:25:28 +0200

    BridgeLogic / BGM

---
+++ Davrobnoesim/Assets/_Game/Scripts/Logic/BridgeLogic.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class BridgeLogic : MonoBehaviour
+{
+    [SerializeField] private GameObject bridgeOn;
+    [SerializeField] private GameObject bridgeOff;
+    [SerializeField] private bool enableBridge;
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if (enableBridge == true)
+            {
+                bridgeOn.SetActive(true);
+                bridgeOff.SetActive(false);
+            }
+            else
+            {
+                bridgeOff.SetActive(true);
+                bridgeOn.SetActive(false);
+            }
+        }
+    }
+}
