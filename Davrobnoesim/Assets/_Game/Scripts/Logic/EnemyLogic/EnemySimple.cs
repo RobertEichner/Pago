9c7814abcdacebe95a55c553c43f765e6ce85cf4
Robert Eichner <roberteichner3@web,de>
2020-08-09 13:16:15 +0200

    Reset und Knockback und Münzen nicht auf collider

--- Davrobnoesim/Assets/_Game/Scripts/Logic/EnemyLogic/EnemySimple.cs
+++ Davrobnoesim/Assets/_Game/Scripts/Logic/EnemyLogic/EnemySimple.cs
@@ -6,23 +6,27 @@ using Random = UnityEngine.Random;
 
 public class EnemySimple : MonoBehaviour, IDamagable
 {
-
+    [Header("Enemy Stats")]
     [SerializeField] private int health = 100;
-    [SerializeField] private ParticleSystem[] partSys;
     [SerializeField] private float speed = 3f;
     [SerializeField] private float attentionRange = 4f;
     [SerializeField] private float attackCooldown = 2f;
     [SerializeField] private float attackRange = 1f;
     [SerializeField] private int attackDamage = 100;
-    [SerializeField] private LayerMask layerMask = -1;
+    [SerializeField] private LayerMask layerMaskToHit = -1;
+    [Header("EnemyDrop Logic")]
     [SerializeField] private Item[] drop = null;
     [SerializeField] private ItemPrefab.ItemState[] state = null;
-    
+    [SerializeField] private LayerMask layerToBlockDrop;
+    [Header("Enemy effects")]
+    [SerializeField] private ParticleSystem[] partSys;
     private bool canAttack = true;
     private Transform target = null;
     private CapsuleCollider2D col;
     private Animator anim = null;
-    
+    private float knockbackDur = 0.5f;
+    private float currentKnockback = 0;
+
     // Start is called before the first frame update
     void Awake()
     {
@@ -41,21 +45,28 @@ public class EnemySimple : MonoBehaviour, IDamagable
             return;
 
         LookAtTarget(direction);
-        transform.position += direction.normalized * (Time.deltaTime * speed);
-        
+        if (currentKnockback >= Mathf.Epsilon)
+        {
+            currentKnockback -= Time.deltaTime;
+            transform.position -= direction.normalized * (Time.deltaTime * speed);
+        }
+        else
+        {
+            transform.position += direction.normalized * (Time.deltaTime * speed);   
+        }
+
         if(dist > attackRange)
             return;
         
         Attack(direction);
-           
-        
-        
+
     }
 
     public void DealDamage(int amount)
     {
         health -= amount;
         Instantiate(partSys[0], transform.position, Quaternion.identity, transform);
+        currentKnockback = currentKnockback >= Mathf.Epsilon ? currentKnockback : knockbackDur;
         if (health <= 0)
         {
             Instantiate(partSys[1], transform.position, Quaternion.identity);
@@ -69,7 +80,7 @@ public class EnemySimple : MonoBehaviour, IDamagable
         if(!canAttack)
             return;
         
-        RaycastHit2D hit = Physics2D.Raycast(transform.position, direction.normalized, 2f, layerMask);
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, direction.normalized, 2f, layerMaskToHit);
         
         if(!hit)
             return;
@@ -110,13 +121,36 @@ public class EnemySimple : MonoBehaviour, IDamagable
             throw new Exception("ArrayLänge von Drop und State stimmen nicht überein");
         for (int i = 0; i < drop.Length; i++)
         {
-            float xOff = Random.value;
-            float yOff = Random.value;
+            float xOff = Random.Range(-1f, 1f);
+            float yOff = Random.Range(-1f, 1f);
             Vector3 offset = new Vector3(xOff, yOff, 0);
             GameObject itemToSpawn = Resources.Load<GameObject>("Prefabs/WorldItemCollectable");
             if (itemToSpawn.TryGetComponent<ItemPrefab>(out var itemPrefab))
                 itemPrefab.UpdateItem(drop[i], state[i]);
-            Instantiate(itemToSpawn, transform.position + offset, Quaternion.identity);
+            
+            if(!Physics2D.Raycast(transform.position, (transform.position + offset) - transform.position, 2f, layerToBlockDrop))
+                Instantiate(itemToSpawn, transform.position + offset, Quaternion.identity);
+            else
+            {
+                Vector3 newPos = target.position - (transform.position + offset);
+                Instantiate(itemToSpawn, RayCastCrossPlayer(newPos), Quaternion.identity);
+            }
         }
     }
+
+    private Vector3 RayCastCrossPlayer(Vector3 direction)
+    {
+        if (!Physics2D.Raycast(target.position, direction, 2f, layerToBlockDrop))
+            return target.position + direction.normalized;
+        if (!Physics2D.Raycast(target.position, -direction, 2f, layerToBlockDrop))
+            return target.position - direction.normalized;
+        Vector3 rotDir = Quaternion.AngleAxis(45f, Vector3.up) * direction;
+        if (!Physics2D.Raycast(target.position, rotDir, 2f, layerToBlockDrop))
+            return target.position + rotDir.normalized;
+        if (!Physics2D.Raycast(target.position, -rotDir, 2f, layerToBlockDrop))
+            return target.position - rotDir.normalized;
+
+        return target.position;
+    }
+    
 }
