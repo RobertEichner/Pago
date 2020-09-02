278cbdd4749f7715374f69725e2f1d4333c4f9ad
Robert Eichner <roberteichner3@web,de>
2020-09-02 11:06:29 +0200

    ungenutze Scenen gelöscht/Quest v1

--- Davrobnoesim/Assets/_Game/Scripts/Logic/QuestLogic/Quest.cs
+++ Davrobnoesim/Assets/_Game/Scripts/Logic/QuestLogic/Quest.cs
@@ -8,6 +8,21 @@ public class Quest: ScriptableObject
     [SerializeField] private string name;
     [SerializeField] private string description;
     [SerializeField] private int questId = -1;
+    [SerializeField] private QuestTask task;
+    private bool isDone = false;
+    public string Name => name;
+
+    public string Description => description;
+
+    public int QuestId => questId;
+
+    public QuestTask Task => task;
+
+    public bool IsDone
+    {
+        get => isDone;
+        set => isDone = value;
+    }
 
 
 }
--- Davrobnoesim/Assets/_Game/Scripts/Logic/QuestLogic/QuestManager.cs
+++ Davrobnoesim/Assets/_Game/Scripts/Logic/QuestLogic/QuestManager.cs
@@ -1,19 +1,52 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class QuestManager : MonoBehaviour
 {
-    private 
-    // Start is called before the first frame update
-    void Start()
+    private List<Quest> quests = new List<Quest>();
+
+    [SerializeField] private Quest testQuest = null;
+    private void Awake()
     {
-        
+        AddQuest(testQuest);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        foreach (var q in quests)
+        {
+            if(!q.IsDone)
+                q.Task.CheckTask();
+        }
+    }
+
+    public bool HasQuest(Quest q)
+    {
+        return quests.Find(item => q == item);
+    }
+    
+    public void AddQuest(Quest q)
+    {
+        if (!HasQuest(q))
+        {
+            quests.Add(q);
+            q.Task.BeginTask();
+        }
+    }
+
+    public bool IsQuestDone(Quest q)
+    {
+        return q.IsDone;
+    }
+
+    public void FinishQuest(Quest q)
+    {
+        if (IsQuestDone(q))
+        {
+            quests.Remove(q);
+            q.Task.EndTask();
+        }
     }
 }
