5bc8338f41925baee78fbc9a3e37ffe51c96171e
Robert Eichner <roberteichner3@web,de>
2020-09-10 13:32:01 +0200

    Dialog schließen, wenn noch nicht abgeschlossen

---
+++ Davrobnoesim/Assets/_Game/Scripts/Logic/DialogLogic/QuestAllInOne.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestAllInOne : State
+{
+    private State stateToChose = null;
+    private bool hasQuest = false;
+    
+    [SerializeField] private QuestTextGiver giveQuest = null;
+    [SerializeField] private QuestTextChecker checkQuest = null;
+    [SerializeField] private QuestFinisher finishQuest = null;
+
+    public override State[] GetNextStates()
+    {
+        State[] toReturn = new State[1];
+        toReturn[0] = stateToChose;
+        return toReturn;
+    }
+
+    public override void StoryEvent()
+    {
+    }
+}
