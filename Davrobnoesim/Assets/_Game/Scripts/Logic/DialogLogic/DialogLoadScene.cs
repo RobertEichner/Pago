f649e550111c84502403e2fbb60bedfdfd0bfc82
Robert Eichner <roberteichner3@web,de>
2020-09-11 17:22:56 +0200

    Dialog der Scene lädt

---
+++ Davrobnoesim/Assets/_Game/Scripts/Logic/DialogLogic/DialogLoadScene.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[CreateAssetMenu(menuName = "Dialog/LoadScene")]
+public class DialogLoadScene : State
+{
+    [SerializeField] private int sceneNumber = 0;
+
+    public override void StoryEvent()
+    {
+        SceneManager.LoadScene(sceneNumber);
+    }
+}
