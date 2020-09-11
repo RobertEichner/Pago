40d8f131a40a72da42a14f06f1c4a3c519c7f9c4
Robert Eichner <roberteichner3@web,de>
2020-09-11 17:18:26 +0200

    Dialog der prüft ob x Quest abgeschlossen sind(Carl)

---
+++ Davrobnoesim/Assets/_Game/Scripts/Logic/DialogLogic/FinishXQuests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Dialog/FinishedXQuests")]
+public class FinishXQuests : DialogToFinish
+{
+    [SerializeField] private Quest[] quests;
+    public override void StoryEvent()
+    {
+        isDone = CanBeDone();
+    }
+
+    public override bool CanBeDone()
+    {
+        foreach (var quest in quests)
+        {
+            if (!quest.IsDone)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
