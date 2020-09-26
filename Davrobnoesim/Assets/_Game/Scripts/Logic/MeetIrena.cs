dace363be55d73559c1cd7be40300d482a002d2d
Robert Eichner <roberteichner3@web,de>
2020-09-26 10:14:57 +0200

    Irena treffen Quest

---
+++ Davrobnoesim/Assets/_Game/Scripts/Logic/MeetIrena.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeetIrena : MonoBehaviour
+{
+    [SerializeField] private Quest quest;
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player") && !quest.IsDone)
+        {
+            QuestManager qm = other.GetComponent<QuestManager>();
+            qm.AddQuest(quest);
+            qm.FinishQuest(quest);
+        }
+    }
+}
---
+++ Davrobnoesim/Assets/_Game/Scripts/Logic/QuestLogic/QuestTasksScripts/EnterTask.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Quest/EnterTask erstellen")]
+public class EnterTask : QuestTask
+{
+    public override bool CheckTaskDone()
+    {
+        return true;
+    }
+
+    public override void BeginTask()
+    {
+    }
+
+    public override void EndTask()
+    {
+    }
+}
